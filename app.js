cd2022bd0e0c6b445f44c379f752dcd1add9da1f
kaushal solanki <kaushalsolanki1999@gmail.com>
2021-11-29 14:14:20 -0500

    User Login Side

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const configRoutes = require('./routes');
 const static = express.static(__dirname + '/public');
 const exphbs = require('express-handlebars');
+const session = require('express-session');
 app.use('/public', static);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -10,7 +11,16 @@ app.use(express.urlencoded({ extended: true }));
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
+app.use(
+    session({
+      name: 'AuthCookie',
+      secret: "some secret string!",
+      saveUninitialized: true,
+      resave: false,
+    })
+  );
 
+  
 configRoutes(app);
 
 app.listen(3000, () => {
